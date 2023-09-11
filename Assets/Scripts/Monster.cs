5335bf0b39a2f779b914851349d082fa296d70de
gokmentas <56593104+gokmentas@users.noreply.github.com>
2023-09-11 20:57:25 +0300

    Writed enemy script

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    [HideInInspector]
+    private Rigidbody2D myBody;
+
+    public float speed;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+
+        speed = 7;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        myBody.velocity = new Vector2(speed, myBody.velocity.y);
+
+    }
+
+} // class
